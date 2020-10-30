9519a303040992301f7c0247631f7225e40d2f57
emanmacario <emacario1997@hotmail.com>
2020-10-30 23:04:19 +1100

    Initial commit

---
+++ src/main/java/com/emanmacario/expensely/ExpenselyApplication.java
@@ -0,0 +1,13 @@
+package com.emanmacario.expensely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenselyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenselyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emanmacario/expensely/ExpenselyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanmacario.expensely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenselyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
